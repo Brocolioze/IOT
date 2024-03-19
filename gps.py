cf9e9409831d4f222e9f2c3ce57a49e0bb8f4cc3
Brocolioze <49524886+Brocolioze@users.noreply.github.com>
2024-03-19 11:59:56 -0400

    maj_deux

--- gps.py
+++ gps.py
@@ -4,14 +4,17 @@ import pynmea2
 def parseGPS(data):
     if data.find(b'GGA') > 0:
         msg = pynmea2.parse(data.decode())
-        print("Timestamp: {} -- Lat: {} {} -- Lon: {} {} -- Altitude: {} {}".format(msg.timestamp, msg.lat, msg.lat_dir, msg.lon, msg.lon_dir, msg.altitude, msg.altitude_units))
+        return "{},{},{},{},{}".format(msg.timestamp, msg.lat, msg.lat_dir, msg.lon, msg.lon_dir)
 
 serialPort = serial.Serial("/dev/ttyAMA0", 9600, timeout=0.5)
 
-while True:
-    try:
-        data = serialPort.readline()
-        if data:
-            parseGPS(data)
-    except Exception as e:
-        print("Error:", e)
+with open("gps_data.txt", "w") as file:
+    while True:
+        try:
+            data = serialPort.readline()
+            if data:
+                gps_data = parseGPS(data)
+                if gps_data:
+                    file.write(gps_data + "\n")
+        except Exception as e:
+            print("Error:", e)
